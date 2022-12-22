cec05d0b623f804be87ee063b32b72acbe591ea0
Vladislav Mekhonoshin <vladmexon@gmail.com>
2022-12-22 17:05:31 +0500

    Server updated

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from flask import Flask
 from flask_restful import Api, Resource
-import requests
 
-lightStatus = False
+def LightOn(): #включить свет
+    pass
+
+def LightOff(): #выключить свет
+    pass
+
+def GetLightStatus(): #получить статус света
+    return False
+    pass
+
+
 class LightUpdate(Resource):
-    global lightStatus
     def post(self, id=0):
        if (id == 1):
-        lightStatus = True
-        print("on")
+        LightOn()
        elif(id == 0):
-        print("off")
-        lightStatus = False
+        LightOff()
        elif(id == 2):
-        lightStatus = not lightStatus
-        print("switch to different")
+        if(GetLightStatus()):
+            LightOff()
+        else:
+            LightOn()
     def get(self):
-        return lightStatus, 200
+        return GetLightStatus(), 200
 
 if __name__ == '__main__':
     app = Flask(__name__)
